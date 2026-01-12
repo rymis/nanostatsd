b662a5334d622048b2b7e3da221067f382e1d98e
Mikhail Ryzhov <rymiser@gmail.com>
2026-01-12 23:43:43 +0100

    Add custom JSON encoder for Quant

--- metricdb/quant.go
+++ metricdb/quant.go
@@ -1,6 +1,9 @@
 package metricdb
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 // Quant is a timestamp in 15 second intervals from UNIX epoch
 type Quant uint32
@@ -18,4 +21,21 @@ func TimeToQuant(t time.Time) Quant {
 // Convert quant to timestamp
 func QuantStamp(q Quant) time.Time {
 	return time.Unix(int64(q) * 15, 0)
+}
+
+func (q Quant) MarshalJSON() ([]byte, error) {
+	v := float64(q) * 15.0
+	return json.Marshal(v)
+}
+
+func (q *Quant) UnmarshalJSON(data []byte) error {
+	var v float64
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return err
+	}
+
+	*q = Quant(v / 15.0)
+
+	return nil
 }
